8769a7081a4f91776cc1bc0d3efb4d6e690d7348
ShavaughnAD <shavaughnd1@gmail.com>
2020-04-11 15:45:51 -0400

    Adding Post Processing
    - Added Post Processing To The Player Camera for Better Lighting
    - Moved files to Different Folders

--- Psych/Assets/Scripts/FloatingPopUp.cs
+++ Psych/Assets/Scripts/FloatingPopUp.cs
@@ -1,20 +1,18 @@
-﻿    using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FloatingPopUp : MonoBehaviour
 {
     public float destroytime = 3f;
     public Vector3 offset = new Vector3(0, 2, 0);
     public Vector3 randomizeIntensity = new Vector3(0.5f, 0, 0);
-    // Start is called before the first frame update
+
     void Start()
     {
         Destroy(gameObject, destroytime);
 
         transform.localPosition += offset;
         transform.localPosition += new Vector3(Random.Range(-randomizeIntensity.x, randomizeIntensity.x),
-            Random.Range(-randomizeIntensity.y, randomizeIntensity.y),
-            Random.Range(-randomizeIntensity.z, randomizeIntensity.z));
+        Random.Range(-randomizeIntensity.y, randomizeIntensity.y),
+        Random.Range(-randomizeIntensity.z, randomizeIntensity.z));
     }
 }
