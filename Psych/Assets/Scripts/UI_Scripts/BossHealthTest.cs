0b5cf675ba2ae61953328456cd05e0748e912982
Sono528 <Sono528@users.noreply.github.com>
2020-05-10 17:12:47 -0400

    Boss Health Bar implimented
    -Added the boss health bar collder and UI
    -Added a placeholder skull for an icon

---
+++ Psych/Assets/Scripts/UI_Scripts/BossHealthTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealthTest : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    public GameObject bossHealthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetBossMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+
+        if (currentHealth <= 0)
+        {
+            bossHealthBar.SetActive(false);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetBossHealth(currentHealth);
+    }
+}
