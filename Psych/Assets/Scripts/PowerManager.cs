71f30d820b0045c32d3d2b797dc4dbc1282fe079
KishoreBoopathi <60489284+KishoreBoopathi@users.noreply.github.com>
2020-05-19 16:18:10 -0400

    Update of power bar mechanism

--- Psych/Assets/Scripts/PowerManager.cs
+++ Psych/Assets/Scripts/PowerManager.cs
@@ -1,26 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerManager : MonoBehaviour
 {
 
     [SerializeField] float power = 50f;
     GameObject bar;
+    GameObject powerRadar;
     bool isStasis = false;
     float timer = 0f;
     float secondCount = 1f;
 
+    Color normalMode = new Color(0f, 0.2878585f, 1f, 1f);
+    Color stasisMode = new Color(0.7137255f, 0.2313726f, 1f, 1f);
+
     private void Start()
     {
         bar = GameObject.Find("Bar");
+        powerRadar = GameObject.Find("PowerRadar");
+        powerRadar.GetComponent<Image>().color = normalMode;
     }
 
     private void Update()
     {
-        timer += Time.deltaTime;
+        if (power <= 0)
+        {
+            isStasis = true;
+            powerRadar.GetComponent<Image>().color = stasisMode;
+        }
 
-        if(timer > secondCount)
+        timer += Time.deltaTime;
+        if (timer > secondCount)
             PowerUp();
     }
 
@@ -34,7 +46,6 @@ public class PowerManager : MonoBehaviour
         }
         else
         {
-            isStasis = true;
             Debug.LogError("Not enough Power");
             return false;
         } 
@@ -54,6 +65,7 @@ public class PowerManager : MonoBehaviour
         }
         else
         {
+            powerRadar.GetComponent<Image>().color = normalMode;
             isStasis = false;
         }
     }
