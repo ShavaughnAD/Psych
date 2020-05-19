c46c9d5243e54e259b06a2b215849768c08d6b86
Raj Shah <rvshah269@gmail.com>
2020-05-19 12:54:14 -0400

    Double Turret

---
+++ Psych/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float force = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * force);
+    }
+}
