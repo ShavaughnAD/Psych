9273d1060ee9ec8c20516b97225110ddbfdd586c
T-Rich23 <58788425+T-Rich23@users.noreply.github.com>
2020-08-26 16:37:01 -0400

    Added Script for Slug Scientist

---
+++ Psych/Assets/EnemySci.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySci : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public GameObject player;
+    public float fov = 94.0f;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+    void Update()
+    {
+       if(PlayerVisible())
+       {
+            anim.Play("SlugScientistRunawayAnim");
+         
+       } 
+    }
+    bool PlayerVisible()
+    {
+        bool visib = false;
+        Vector3 direction = player.transform.position - transform.position;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,direction, out hit, 10f))
+        {
+            if (hit.transform.tag.Equals(player.transform.tag))
+            {
+                float angle = Vector3.Angle(transform.forward, direction);
+                if(angle <= fov)
+                {
+                    visib = true;
+                }
+            }
+        }
+        return visib;
+    }
+}
