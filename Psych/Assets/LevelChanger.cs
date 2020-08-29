970c2960311a8464d3457eadd900ee36ef229ed4
RonnieDS <58788535+RonnieDS@users.noreply.github.com>
2020-08-29 19:53:55 -0400

    levelchanger script w/fading
    -Made a script to fade in and out of inbetween scenes when transitioning.
    -Made a prefab of "levelchanger" so it works on all scenes.
    -Made two animations one fades in the other fades out.

---
+++ Psych/Assets/LevelChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+    
+    public void FadeToLevel (int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("Fadeout");       
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
