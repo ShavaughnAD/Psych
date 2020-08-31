301fcaaf64df11d0995953e602a4142af8fe1157
Leifeon001 <tristen.howk@yahoo.com>
2020-08-31 09:35:48 -0400

    GunRack numbers
    Number of weapons in GunRack

--- Psych/Assets/Scripts/GunRack.cs
+++ Psych/Assets/Scripts/GunRack.cs
@@ -5,19 +5,23 @@ public class GunRack : MonoBehaviour
     //public GameObject weaponSelection;
     //public GameObject[] weapons;
     //public Transform weaponSpawnPoint;
-
+    public int CurInRack;
     void OnTriggerEnter(Collider other)
     {
         //if(other.tag == "Player")
         //{
         //    weaponSelection.SetActive(true);
         //}
-        if(other.tag == "Enemy" && other.GetComponent<AttackState>().currentWeapon == null)
+        if(other.tag == "Enemy" && other.GetComponent<AttackState>().currentWeapon == null && CurInRack > 0)
         {
             AttackState enemyAttackState = other.GetComponent<AttackState>();
             enemyAttackState.currentWeapon = Instantiate(enemyAttackState.C_Stolen, enemyAttackState.GunHand.position, new Quaternion(0,0,0,0), enemyAttackState.GunHand);
             enemyAttackState.currentWeapon.transform.localScale = new Vector3(0.00007f, 0.00007f, 0.00007f);
             enemyAttackState.currentWeapon.transform.localEulerAngles = enemyAttackState.storedWeaponEulerAngleRotation;      
+        }
+        else if(CurInRack < 0)
+        {
+            //Panic
         }
     }
 
