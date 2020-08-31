abbb3269e393a265bbee01a1de86337cacaf1aa4
jtrev <jtrev23@gmail.com>
2020-08-31 08:57:45 -0400

    Able to mostly get camera wall slide working

--- Psych/Assets/Scripts/CameraScripts/CameraWallSlide.cs
+++ Psych/Assets/Scripts/CameraScripts/CameraWallSlide.cs
@@ -6,7 +6,6 @@ public class CameraWallSlide : MonoBehaviour
 {
     [SerializeField]
     Camera myCamera = null;
-    private Vector3 previousPlayerPosition;
     private Rigidbody myRigidbody;
     private RaycastHit hitBox;
     [SerializeField]
@@ -21,7 +20,7 @@ public class CameraWallSlide : MonoBehaviour
     public Vector3 previousCameraPosition;
     public Vector3 previousCameraRotation;
     public float lerpSpeed = 2.0f;
-    public float maxDragDistance = 5.0f;
+    public float wallDetectionDistace = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,7 +35,6 @@ public class CameraWallSlide : MonoBehaviour
   
     private void LateUpdate()
     {
-        previousPlayerPosition = playerMovementScript.transform.position;
         previousCameraPosition = transform.localPosition;
         previousCameraRotation = transform.localEulerAngles;
     }
@@ -56,34 +54,77 @@ public class CameraWallSlide : MonoBehaviour
   
                 Vector3 newloc = Vector3.zero;
                 newloc = transform.localPosition + Vector3.forward * sphereCheckRadius;
-                if (Vector3.Distance(newloc, playerMovementScript.transform.position) < maxDragDistance)
+                float distance = Vector3.Distance(newloc, playerMovementScript.transform.position);
+                if (distance < wallDetectionDistace)
                 {
-                    transform.localPosition = Vector3.Slerp(transform.localPosition, newloc, lerpSpeed * playerMovementScript.speed * Time.fixedDeltaTime);
+                    transform.localPosition = Vector3.Lerp(transform.localPosition, newloc, lerpSpeed  * Time.fixedDeltaTime);
+                   // Debug.Log("1");
+                }
 
-                }            
             }
             else if (Physics.SphereCast(myCamera.transform.position, sphereCheckRadius * 2, checkBehind, out hitBox))
             {
+                //Keep this empty, don't want to move camera if its hugged against wall.
+
             }
             else if (Physics.OverlapSphere(transform.position, sphereCheckRadius).Length > 0)
             {
                 Vector3 newloc = transform.localPosition + Vector3.forward * sphereCheckRadius;
+                float distance = Vector3.Distance(newloc, playerMovementScript.transform.position);
 
-                transform.localPosition = Vector3.Slerp(transform.localPosition, newloc, lerpSpeed * playerMovementScript.speed * Time.fixedDeltaTime);
+                if (distance < wallDetectionDistace)
+                {
+                    transform.localPosition = Vector3.Lerp(transform.localPosition, newloc, lerpSpeed * Time.fixedDeltaTime);
+                    //Debug.Log("3");
+                }
             }
             else if (Physics.OverlapSphere(transform.position, sphereCheckRadius * 2).Length > 0)
             {
            //Keep this empty, don't want to move camera if its hugged against wall.
+                    Debug.Log("4");
+            }
+
+           else if (Physics.SphereCast(playerMovementScript.transform.position, sphereCheckRadius, checkBehind, out hitBox))
+            {
+                transform.localEulerAngles = previousCameraRotation;
+
+                Vector3 newloc = Vector3.zero;
+                newloc = targetLocation + Vector3.forward * sphereCheckRadius;
+                float distance = Vector3.Distance(newloc, playerMovementScript.transform.position);
+                    Debug.Log("1");
+                if (distance < wallDetectionDistace)
+                {
+                    transform.localPosition = Vector3.Lerp(transform.localPosition, newloc, lerpSpeed * Time.fixedDeltaTime);
+                    Debug.Log("10");
+                }
+
+            }
+
+            else if (Physics.OverlapSphere(playerMovementScript.transform.position, sphereCheckRadius).Length > 0)
+            {
+                Vector3 newloc = targetLocation + Vector3.forward * sphereCheckRadius;
+                float distance = Vector3.Distance(newloc, playerMovementScript.transform.position);
+
+                    Debug.Log("30");
+                if (distance < wallDetectionDistace)
+                {
+                    transform.localPosition = Vector3.Lerp(transform.localPosition, newloc, lerpSpeed * Time.fixedDeltaTime);
+                    Debug.Log("3");
+                }
+            }
+            else if (Physics.OverlapSphere(playerMovementScript.transform.position, sphereCheckRadius * 2).Length > 0)
+            {
+                //Keep this empty, don't want to move camera if its hugged against wall.
+                //  Debug.Log("4");
             }
             else
             {
-                transform.localPosition = Vector3.Slerp(transform.localPosition, targetLocation, lerpSpeed * playerMovementScript.speed * Time.fixedDeltaTime);
+                float distance = Vector3.Distance(transform.localPosition, targetLocation);
+                transform.localPosition = Vector3.Lerp(transform.localPosition, targetLocation, lerpSpeed * Time.fixedDeltaTime);
             }
 
-       
 
 
-         
 
         }
     }
