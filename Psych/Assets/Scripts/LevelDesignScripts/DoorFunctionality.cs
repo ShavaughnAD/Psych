3f9304928655c012eddf20243445d64cbb9e13ce
RonnieDS <58788535+RonnieDS@users.noreply.github.com>
2020-09-01 19:39:23 -0400

    Adding audio/modified prefabs
    -got audio for mechanical door, playerhurt, enemyhurt.
    -added audio for mechanical door, playerhurt, enemyhurt.
    -modified PlayerHealth script, EnemyHealth, Doubledoor script to add the audio for the given game object.

--- Psych/Assets/Scripts/LevelDesignScripts/DoorFunctionality.cs
+++ Psych/Assets/Scripts/LevelDesignScripts/DoorFunctionality.cs
@@ -6,6 +6,9 @@ public class DoorFunctionality : MonoBehaviour
     public bool shouldOpen = false;
     public bool Powered;
     public bool BossDoor;
+
+    AudioSource auSource;
+    public AudioClip MechanicalDoor;
     void Awake()
     {
         if(doorAnimator == null)
@@ -20,7 +23,8 @@ public class DoorFunctionality : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player" && Powered || other.tag == "Enemy" && Powered)
-        {
+        {           
+            auSource.PlayOneShot(MechanicalDoor);
             shouldOpen = !shouldOpen;
             doorAnimator.SetBool("open", shouldOpen);
         }
